a6d78ca61d25e3330bd5b6f99196fccbe7d62ed1
danydifilippo <danydifilippo@gmail.com>
2022-09-14 23:14:16 +0200

    almost complete

--- js/script.js
+++ js/script.js
@@ -1,17 +1,33 @@
 
-
 function resultAge() {
     let a,b;
     a= Number(document.getElementById('age1').value);
     b= Number(document.getElementById('age2').value);
-    let result
-    result=a-b;
+    let result1
+    result1=a-b;
 
-if (result>=0) {
-    //document.getElementById('vecchio').style.visibility=visibility
-    document.write('si')
+function healthyLife () {
+    let c,d,e,f;
+    c= Number(document.getElementById('eat').value);
+    d= Number(document.getElementById('sport').value);
+    e= Number(document.getElementById('alcool').value);
+    f= document.getElementById('genreF');
+    for (let checkbox of f)
+        if (checkbox.checked){
+            f=1
+        }
+        else {
+            f=0
+        }
+    let result2
+    result2=(c+d+e+f)
+}
+
+if (result1>=0 || result2<6) {
+    document.write.getElementById('gif_age_old').style.visibility = visible
 } else {
-    //document.getElementById('giovane').style.display = inline
-    document.write('no')
+    document.write.getElementById('gif_age_young').style.visibility = visible
 }
 }
+
+
