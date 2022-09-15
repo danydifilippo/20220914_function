4f6feac7ff9238ce9b2b3cc59e1da0b0272a4e8c
danydifilippo <danydifilippo@gmail.com>
2022-09-15 15:22:37 +0200

    final.complete

--- js/script.js
+++ js/script.js
@@ -1,33 +1,30 @@
 
+
 function resultAge() {
     let a,b;
-    a= Number(document.getElementById('age1').value);
-    b= Number(document.getElementById('age2').value);
-    let result1
-    result1=a-b;
-
-function healthyLife () {
+    a = Number(document.getElementById('age1').value);
+    b = Number(document.getElementById('age2').value);
+    let result1 = a-b
     let c,d,e,f;
     c= Number(document.getElementById('eat').value);
     d= Number(document.getElementById('sport').value);
     e= Number(document.getElementById('alcool').value);
     f= document.getElementById('genreF');
-    for (let checkbox of f)
-        if (checkbox.checked){
+
+        if (f.checked){
             f=1
         }
         else {
             f=0
-        }
-    let result2
-    result2=(c+d+e+f)
-}
+        };
+    let result2 = c+d+e+f;
 
-if (result1>=0 || result2<6) {
-    document.write.getElementById('gif_age_old').style.visibility = visible
-} else {
-    document.write.getElementById('gif_age_young').style.visibility = visible
+    if ((result1>=0) || (result2<5)) {
+        document.getElementById('gif_age_old').style.visibility="visible";
+        document.getElementById('main_').style.display="none";
+    } else {
+        document.getElementById('gif_age_young').style.visibility="visible";
+        document.getElementById('main_').style.display="none";
+    }
 }
-}
-
 
